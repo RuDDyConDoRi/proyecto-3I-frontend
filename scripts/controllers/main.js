17374b80933941b681e6529212785029b275c8ba
RuDDyConDoRi <rucocool@hotmail.com>
2018-07-25 14:46:11 -0430

    conectando con servicio para iniciar el agente AgenteResolutorCierre

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -1,5 +1,4 @@
 'use strict';
-
 /**
  * @ngdoc function
  * @name frontendApp.controller:MainCtrl
@@ -10,7 +9,20 @@
 angular.module('frontendApp')
   .controller('MainCtrl', ['$scope', '$http', 
   	function($scope, $http){
+      
       $scope.datos_servicio = "";
+      $scope.respuesta = "";
+
+      $scope.datos = {
+          atributos:["Ruta","Empresa","Tiempo","Distancia","Prisa"],
+          tabla:[["2S","A","20","05","SI","SI"],
+                 ["03","B","30","10","SI","NO"],
+                 ["2S","A","10","15","SI","SI"],
+                 ["4B","C","20","10","NO","SI"],
+                 ["03","B","20","05","SI","SI"],
+                 ["4B","A","30","15","SI","NO"],
+                 ["03","B","10","30","SI","NO"]]
+        }
       
       $scope.llamar = function(){
         $http({
@@ -28,27 +40,17 @@ angular.module('frontendApp')
       };
     
 
-    $scope.llamarAgente = function(){
-
-        $scope.usuario = {
-          nombre: "root",
-          contrasena: "root",
-          accion: "validar"
-        }
-
-        $http({
-          method: "POST",
-          url: "http://localhost:8080/JadeEjemplo001/ServletValidar",
-          params: $scope.usuario
-        }).then(function Success(response) {
+      $scope.llamarAgente = function(){
+        $http.post("http://localhost:8080/iii/agente/cierre", $scope.datos)
+        .then(function Success(response) {
           $scope.myRes = response.data;
           $scope.statuscode = response.status;
           console.log('Respuesta: ', response.data);
-          $scope.datos_servicio = response.data.tasks;
+          $scope.respuesta = response.data;
         }, function Error(response) {
           $scope.myRes = response.myArrayList;
           console.log('Error: ', response);
         });
       };
-  }
+    }
 ]);
